7e20eb768ddfa38a340c290fce9e011535dfab89
Miro1028 <mglanimous@gmail.com>
2022-06-19 14:13:53 +0800

    calculator
    Vanilla javascript calculator

---
+++ index.js
@@ -0,0 +1,6 @@
+var input = document.getElementById('input'),
+    numbers = document.querySelectorAll('.numbers div'),
+    operators = document.querySelectorAll('.numbers div'),
+    result = document.getElementById('result')
+    clear = document.getElementById('clear'),
+    resultDisplayed = false;
